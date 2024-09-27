91602b8780ed2cc13dd7bd4a2c0843beceffdea5
ChefZander <tobias.aha2008@gmail.com>
2024-09-27 22:47:24 +0200

    uci engine basics

--- src/UCIEngine.java
+++ src/UCIEngine.java
@@ -2,33 +2,87 @@ import java.io.*;
 
 public class UCIEngine {
     private String engineCommand;
-    private ProcessBuilder engineProcess;
+
+    private ProcessBuilder processBuilder;
+    private Process engineProcess;
 
     private BufferedReader reader;
     private BufferedWriter writer;
 
+    /**
+     * Create a new UCIEngine instance
+     * @param engineCommand The command to run, to get a UCI interface.
+     */
     public UCIEngine(String engineCommand) {
         this.engineCommand = engineCommand;
-        this.engineProcess = new ProcessBuilder(engineCommand);
+        this.processBuilder = new ProcessBuilder(engineCommand);
     }
 
-    public void prepare() {
-        try {
-            // start the engine
-            engineProcess.start();
+    /**
+     * Launch the engine process with the command defined in the constructor, and create stdin and stdout handles.
+     * @throws IOException
+     */
+    public void prepare() throws IOException {
+        // start the engine
+        engineProcess = processBuilder.start();
 
-            // get a handle on stdin
-            OutputStream stdin = engineProcess.getOutputStream();
-            writer = new BufferedWriter(new OutputStreamWriter(stdin));
+        // get a handle on stdin
+        OutputStream stdin = engineProcess.getOutputStream();
+        writer = new BufferedWriter(new OutputStreamWriter(stdin));
 
-            // get a handle on stdout
-            InputStream stdout = engineProcess.getInputStream();
-            reader = new BufferedReader(new InputStreamReader(stdout));
-        }
-        catch (Exception ignored) {}
+        // get a handle on stdout
+        InputStream stdout = engineProcess.getInputStream();
+        reader = new BufferedReader(new InputStreamReader(stdout));
     }
 
-    public void writeLine(String line) {
+    /**
+     * Write a line to the engine process' stdin
+     * @param line The line to write
+     * @throws IOException
+     */
+    public void writeLine(String line) throws IOException {
         writer.write(line + "\n");
+    }
+
+    /**
+     * Read a line from the engine process' stdout
+     * @returns The line read
+     * @throws IOException
+     */
+    public String readLine() throws IOException {
+        return reader.readLine();
+    }
+
+    /**
+     * Set a board position in the engine by FEN String
+     * @param fen The FEN to set
+     * @throws IOException
+     */
+    public void setPositionFen(String fen) throws IOException {
+        this.writeLine("position fen " + fen);
+    }
+
+    /**
+     * Tell the engine to calculate the best move for the position.
+     * @return The best engine in SAN format. (e.g. "a1a3")
+     * @throws IOException
+     */
+    public String go() throws IOException {
+        this.writeLine("go");
+
+        String nextLine = this.readLine();
+        while (nextLine != null) {
+            String[] tokens = nextLine.split(" ");
+            if(tokens[0].equals("bestmove")) return tokens[1];
+
+            if(tokens[0].equals("info")) {
+                // This is used by the engine to send telemetry data.
+                // We will output this to the user via graphs / a terminal.
+            }
+
+            nextLine = this.readLine();
+        }
+
+        return null; // There was no move returned. (Engine process crashed?)
     }
 }
