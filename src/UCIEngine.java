c8d8efea04a89e482bbbd7e4241ad3e941acfc69
ChefZander <tobias.aha2008@gmail.com>
2024-09-27 22:18:15 +0200

    prepare

--- src/UCIEngine.java
+++ src/UCIEngine.java
@@ -1,10 +1,34 @@
+import java.io.*;
+
 public class UCIEngine {
     private String engineCommand;
     private ProcessBuilder engineProcess;
 
+    private BufferedReader reader;
+    private BufferedWriter writer;
+
     public UCIEngine(String engineCommand) {
         this.engineCommand = engineCommand;
+        this.engineProcess = new ProcessBuilder(engineCommand);
     }
 
-    public void 
+    public void prepare() {
+        try {
+            // start the engine
+            engineProcess.start();
+
+            // get a handle on stdin
+            OutputStream stdin = engineProcess.getOutputStream();
+            writer = new BufferedWriter(new OutputStreamWriter(stdin));
+
+            // get a handle on stdout
+            InputStream stdout = engineProcess.getInputStream();
+            reader = new BufferedReader(new InputStreamReader(stdout));
+        }
+        catch (Exception ignored) {}
+    }
+
+    public void writeLine(String line) {
+        writer.write(line + "\n");
+    }
 }
